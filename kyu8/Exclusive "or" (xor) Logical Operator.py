136fb548928a3bd5b65b48128e3a596f3691e66f
gafarchik <50053994+gafarchik@users.noreply.github.com>
2022-06-24 20:48:39 +0300

    Create Exclusive "or" (xor) Logical Operator.py

---
+++ kyu8/Exclusive "or" (xor) Logical Operator.py
@@ -0,0 +1,11 @@
+def xor(a,b):
+    if a and b == True:
+        return False
+    elif a == b:
+        return a
+    elif a == True:
+        return True
+    elif b == True:
+        return True
+    else:
+        return False
