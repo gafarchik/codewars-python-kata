7f5286616382f979e573b61dd64d063026a729f2
gafarchik <50053994+gafarchik@users.noreply.github.com>
2022-06-24 19:36:19 +0300

    Delete Powers_of_2.py

--- Powers_of_2.py
+++
@@ -1,8 +0,0 @@
-def powers_of_two(n):
-    list = [1]
-    for x in range(0,n):
-        if len(list)==1:
-            list.append(2)
-        else:
-            list.append(list[-1]*2)
-    return list
