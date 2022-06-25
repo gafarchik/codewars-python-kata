c509a7a1f470548a97cf5ee6b158d33ae7b61b73
gafarchik <50053994+gafarchik@users.noreply.github.com>
2022-06-25 13:29:28 +0300

    Update The Millionth Fibonacci Kata.py

--- kyu3/The Millionth Fibonacci Kata.py
+++ kyu3/The Millionth Fibonacci Kata.py
@@ -1,17 +1,16 @@
-def fib(n):
-    if n >= 0:
-        return fibiter(1, 0, 0, 1, n)
-    if n < 0:
-        a, b = 0, 1
-        for _ in range(0, n, -1):
-            a, b = b - a, a
-        return a
-
-
-def fibiter(a, b, p, q, count):
-    if count == 0:
-        return b
-    if count % 2 == 0:
-        return fibiter(a, b, p * p + q * q, q * q + 2 * p * q, count / 2)
+def sumfunc(num1, num2, num3, num4, n):
+    if n == 0:
+        return num2
+    if n % 2 == 0:
+        return sumfunc(num1, num2, num3 * num3 + num4 * num4, num4 * num4 + 2 * num3 * num4, n / 2)
     else:
-        return fibiter(b * q + a * q + a * p, b * p + a * q, p, q, count - 1)
+        return sumfunc(num2 * num4 + num1 * num4 + num1 * num3, num2 * num3 + num1 * num4, num3, num4, n - 1)
+
+def fib(n):
+    if n <= 0:
+        num, num2 = 1,0
+        for x in range(0, n, -1):
+            num2, num = num - num2, num2
+        return num2
+    if n > 0:
+        return sumfunc(1, 0, 0, 1, n)
